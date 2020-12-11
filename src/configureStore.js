e1dbff2a2900f3367dc0006e4406f5bf825e5914
ikhvjs <ikhvjs@gmail.com>
2020-12-11 11:12:35 +0100

    set Redux Logger in Dev only

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,11 +3,17 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
 
+//base middleware
+let middleware = [thunkMiddleware];
 
-const logger = createLogger();
+//append middleware for dev Env
+if (process.env.NODE_ENV !== 'prod') {
+  const logger = createLogger();
+  middleware = [...middleware, logger];
+} 
 
 export default function configureStore() {
   return createStore(rootReducer, 
-  	applyMiddleware(thunkMiddleware, logger)
+  	applyMiddleware(...middleware)
   )
 }
